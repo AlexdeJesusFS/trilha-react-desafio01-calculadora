2e0001215f6296cefce847e0e2a4f262aae3af29
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-10-05 02:19:33 -0300

    Criando calcualdora.
    criado os componentes e seu estilo
    estilo da página criado

---
+++ calculator/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 25px;
+    border: 1px solid #CDCDCD;
+    background-color: #0AA0F0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.8;
+    }
+
+`
+
---
+++ calculator/src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: Roboto;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #2947c5;
+        border: 0;
+
+        padding: 0 15px;  
+
+        font-size: 24px;
+        font-familt: Roboto;
+        color: #FFFFFF;
+    }
+`
+
