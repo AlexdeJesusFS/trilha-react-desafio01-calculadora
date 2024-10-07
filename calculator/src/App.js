cff76c36c78a6e4ea8e83116bfdfbafa9f9b8d33
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-10-07 18:50:45 -0300

    função soma e subtração adicionados a calculadora.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,14 +6,60 @@ import { useState } from 'react';
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState(0);
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleOnClear = () => {
-    setCurrentNumber('0')
+    setCurrentNumber('0');
+    setFirstNumber('0');
   };
 
   const handleAddNumber = (number) => {
-    setCurrentNumber(prev => `${number}${prev === '0' ? '' : prev}`)
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`);
+  };
+
+  const handleSumNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+');
+      } else {
+        const sum = Number(firstNumber) + Number(currentNumber);
+        setCurrentNumber(String(sum));
+      };
+  };
+
+  const handleSubNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+      } else {
+        const sum = Number(firstNumber) - Number(currentNumber);
+        setCurrentNumber(String(sum));
+      };
+  };
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation) {
+        case '+':
+          handleSumNumbers();
+          break;
+
+        case '-':
+          handleSubNumbers();
+          break;
+
+        default:
+          break;
+      };
+      
+      } else {
+        const sum = Number(firstNumber) + Number(currentNumber);
+        setCurrentNumber(String(sum));
+      };
   };
 
   return (
@@ -24,25 +70,25 @@ const App = () => {
           <Button label="x" onClick={() => handleAddNumber('x')}/>
           <Button label="/" onClick={() => handleAddNumber('/')}/>
           <Button label="C" onClick={() => handleOnClear()}/>
-          <Button label="." onClick={() => handleAddNumber('.')}/>
+          <Button label="<" onClick={() => handleAddNumber('<')} />
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={() => handleAddNumber('-')}/>
+          <Button label="-" onClick={handleSubNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={() => handleAddNumber('+')}/>
-        </Row>
+          <Button label="+" onClick={handleSumNumbers}/>
+          </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={() => handleAddNumber('=')}/>
+          <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
     </Container>
